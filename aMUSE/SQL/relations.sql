ca38256a395adb31d24fd0e39addcea68d8a8e77
paolo <paolo@paolo.lan>
2013-03-14 16:02:49 +0100

    relazioni aggiornate

--- aMUSE/SQL/relations.sql
+++ aMUSE/SQL/relations.sql
@@ -9,41 +9,53 @@ UserComment(user_id, object_id, comment, verified)
 */
 
 CREATE TABLE `User` (
-`id` UNSIGNED INT PRIMARY KEY AUTO_INCREMENT,
+`user_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 `email` VARCHAR(255) UNIQUE KEY,
-`password` CHAR(32)
-) ENGINE=INNODB, CHARACTER_SET=utf8, COLLATE=utf8_unicode_ci;
+`password` CHAR(32) NOT NULL
+) ENGINE=INNODB;
 
 CREATE TABLE `PersonalPhoto` (
-`id` UNSIGNED INT PRIMARY KEY AUTO_INCREMENT,
+`personal_photo_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 `comment` TEXT
-) ENGINE=INNODB, CHARACTER_SET=utf8, COLLATE=utf8_unicode_ci;
+) ENGINE=INNODB;
 
 CREATE TABLE `Object` (
-`id` UNSIGNED INT PRIMARY KEY AUTO_INCREMENT,
+`object_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 `name` VARCHAR(255) UNIQUE KEY,
-`exhibition` UNSIGNED INT INDEX,
-`section` UNSIGNED INT INDEX
-) ENGINE=INNODB, CHARACTER_SET=utf8, COLLATE=utf8_unicode_ci;
+`exhibition` INT UNSIGNED INDEX,
+`section` INT UNSIGNED INDEX
+) ENGINE=INNODB;
 
 CREATE TABLE `UserPhoto` (
-`user_id` UNSIGNED INT,
-`personal_photo_id` UNSIGNED INT
-) ENGINE=INNODB, CHARACTER_SET=utf8, COLLATE=utf8_unicode_ci;
+`user_id` INT UNSIGNED,
+`personal_photo_id` INT UNSIGNED,
+PRIMARY KEY (`user_id`, `personal_photo_id`),
+CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`),
+CONSTRAINT FOREIGN KEY (`personal_photo_id`) REFERENCES `PersonalPhoto`(`personal_photo_id`)
+) ENGINE=INNODB;
 
 CREATE TABLE `PhotoReference` (
-`personal_photo_id` UNSIGNED INT,
-`object_id` UNSIGNED INT
-) ENGINE=INNODB, CHARACTER_SET=utf8, COLLATE=utf8_unicode_ci;
+`personal_photo_id` INT UNSIGNED,
+`object_id` INT UNSIGNED,
+PRIMARY KEY (`personal_photo_id`, `object_id`),
+CONSTRAINT FOREIGN KEY (`object_id`) REFERENCES `Object`(`object_id`),
+CONSTRAINT FOREIGN KEY (`personal_photo_id`) REFERENCES `PersonalPhoto`(`personal_photo_id`)
+) ENGINE=INNODB;
 
 CREATE TABLE `UserBookmark` (
-`user_id` UNSIGNED INT,
-`object_id` UNSIGNED INT
-) ENGINE=INNODB, CHARACTER_SET=utf8, COLLATE=utf8_unicode_ci;
+`user_id` INT UNSIGNED,
+`object_id` INT UNSIGNED,
+PRIMARY KEY (`user_id`, `object_id`),
+CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`),
+CONSTRAINT FOREIGN KEY (`object_id`) REFERENCES `Object`(`object_id`)
+) ENGINE=INNODB;
 
 CREATE TABLE `UserComment` (
-`user_id` UNSIGNED INT,
-`object_id` UNSIGNED INT,
-`comment` TEXT,
-`verified` BOOLEAN INDEX
-) ENGINE=INNODB, CHARACTER_SET=utf8, COLLATE=utf8_unicode_ci;
+`user_id` INT UNSIGNED,
+`object_id` INT UNSIGNED,
+`comment` TEXT NOT NULL,
+`verified` BOOLEAN NOT NULL INDEX,
+PRIMARY KEY (`user_id`, `object_id`),
+CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`),
+CONSTRAINT FOREIGN KEY (`object_id`) REFERENCES `Object`(`object_id`)
+) ENGINE=INNODB;

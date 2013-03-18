f392a67df7189fb398bf00bf8c81d0a3f529c6a9
paolo <paolo@paolo.lan>
2013-03-18 23:44:34 +0100

    modifiche relezioni

--- aMUSE/SQL/relations.sql
+++ aMUSE/SQL/relations.sql
@@ -1,15 +1,26 @@
 /*
 User(id, email, password)
-PersonalPhoto(id, user_id, commento)
 Object(id, name, exhibition, sector)
+PersonalPhoto(id, user_id, commento)
 UserBookmark(user_id, object_id)
 UserComment(user_id, object_id, comment, verified)
 */
+
+USE `amuse`; 
+/*seleziona il database in cui creare le tabelle*/
 
 CREATE TABLE `User` (
 `user_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 `email` VARCHAR(255) UNIQUE KEY NOT NULL,
 `password` CHAR(32) NOT NULL
+) ENGINE=INNODB;
+
+CREATE TABLE `Object` (
+`object_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(255) UNIQUE KEY NOT NULL,
+`exhibition` INT UNSIGNED NOT NULL,
+`section` INT UNSIGNED NOT NULL,
+INDEX(`exhibition`, `section`)
 ) ENGINE=INNODB;
 
 CREATE TABLE `PersonalPhoto` (
@@ -19,14 +30,6 @@ CREATE TABLE `PersonalPhoto` (
 `comment` TEXT,
 CONSTRAINT FOREIGN KEY (`object_id`) REFERENCES `Object`(`object_id`) ON UPDATE CASCADE,
 CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON UPDATE CASCADE
-) ENGINE=INNODB;
-
-CREATE TABLE `Object` (
-`object_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-`name` VARCHAR(255) UNIQUE KEY NOT NULL,
-`exhibition` INT UNSIGNED NOT NULL,
-`section` INT UNSIGNED NOT NULL,
-INDEX(`exhibition`, `section`)
 ) ENGINE=INNODB;
 
 CREATE TABLE `UserBookmark` (

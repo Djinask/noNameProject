210575645ada61ef6270823fb686cd958e3bc498
paolo <paolo@paolo.lan>
2013-03-18 23:17:58 +0100

    modifiche relezioni

--- aMUSE/SQL/relations.sql
+++ aMUSE/SQL/relations.sql
@@ -2,54 +2,39 @@
 User(id, email, password)
 PersonalPhoto(id, user_id, commento)
 Object(id, name, exhibition, sector)
-PhotoReference(personal_photo_id, object_id)
 UserBookmark(user_id, object_id)
 UserComment(user_id, object_id, comment, verified)
 */
 
 CREATE TABLE `User` (
 `user_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-`email` VARCHAR(255) UNIQUE KEY,
+`email` VARCHAR(255) UNIQUE KEY NOT NULL,
 `password` CHAR(32) NOT NULL
 ) ENGINE=INNODB;
 
 CREATE TABLE `PersonalPhoto` (
 `personal_photo_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-`user_id` INT UNSIGNED,
+`user_id` INT UNSIGNED NOT NULL,
+`object_id` INT UNSIGNED NOT NULL,
 `comment` TEXT,
-CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`)
+CONSTRAINT FOREIGN KEY (`object_id`) REFERENCES `Object`(`object_id`) ON UPDATE CASCADE,
+CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON UPDATE CASCADE
 ) ENGINE=INNODB;
 
 CREATE TABLE `Object` (
 `object_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-`name` VARCHAR(255) UNIQUE KEY,
-`exhibition` INT UNSIGNED,
-`section` INT UNSIGNED,
+`name` VARCHAR(255) UNIQUE KEY NOT NULL,
+`exhibition` INT UNSIGNED NOT NULL,
+`section` INT UNSIGNED NOT NULL,
 INDEX(`exhibition`, `section`)
-) ENGINE=INNODB;
-
-CREATE TABLE `UserPhoto` (
-`user_id` INT UNSIGNED,
-`personal_photo_id` INT UNSIGNED,
-PRIMARY KEY (`user_id`, `personal_photo_id`),
-CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`),
-CONSTRAINT FOREIGN KEY (`personal_photo_id`) REFERENCES `PersonalPhoto`(`personal_photo_id`)
-) ENGINE=INNODB;
-
-CREATE TABLE `PhotoReference` (
-`personal_photo_id` INT UNSIGNED,
-`object_id` INT UNSIGNED,
-PRIMARY KEY (`personal_photo_id`, `object_id`),
-CONSTRAINT FOREIGN KEY (`object_id`) REFERENCES `Object`(`object_id`),
-CONSTRAINT FOREIGN KEY (`personal_photo_id`) REFERENCES `PersonalPhoto`(`personal_photo_id`)
 ) ENGINE=INNODB;
 
 CREATE TABLE `UserBookmark` (
 `user_id` INT UNSIGNED,
 `object_id` INT UNSIGNED,
 PRIMARY KEY (`user_id`, `object_id`),
-CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`),
-CONSTRAINT FOREIGN KEY (`object_id`) REFERENCES `Object`(`object_id`)
+CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON UPDATE CASCADE,
+CONSTRAINT FOREIGN KEY (`object_id`) REFERENCES `Object`(`object_id`) ON UPDATE CASCADE
 ) ENGINE=INNODB;
 
 CREATE TABLE `UserComment` (
@@ -59,6 +44,6 @@ CREATE TABLE `UserComment` (
 `verified` BOOLEAN NOT NULL,
 INDEX(`verified`),
 PRIMARY KEY (`user_id`, `object_id`),
-CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`),
-CONSTRAINT FOREIGN KEY (`object_id`) REFERENCES `Object`(`object_id`)
+CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON UPDATE CASCADE,
+CONSTRAINT FOREIGN KEY (`object_id`) REFERENCES `Object`(`object_id`) ON UPDATE CASCADE
 ) ENGINE=INNODB;

03a4e70a42fd358d46956dcf26d4e9f551a1e6c8
Simone <Halfblood17@gmail.com>
2013-03-21 18:22:05 +0100

    other changes

---
+++ aMUSE/public_html/js/favorite.js
@@ -0,0 +1,24 @@
+(function (){
+	var buttons = document.querySelectorAll('.starbutton > img');
+	var containers = document.querySelectorAll('.starbutton');
+	var startcolor = containers.item(0).style.backgroundColor;
+	var empty = './img/emptystar.png', full = './img/fullstar.png';
+	for(var i = 0; i < buttons.length; i++) {
+		var item = buttons.item(i);
+		item.starStatus = false;
+		item.src = empty;
+		containers.item(i).addEventListener('click', function(e) {
+			var star = e.currentTarget.querySelector('img');
+			if(star.starStatus){
+				star.src = empty;
+				e.currentTarget.style.backgroundColor = startcolor;
+			}
+			else{
+
+				star.src = full;
+				e.currentTarget .style.backgroundColor = "#6495ED";
+			}
+			star.starStatus = !star.starStatus;
+		});
+	}
+})();

b4de70ac544135bcf56697f6c978ca0420eac11c
LucaM <lucchard@gmail.com>
2013-03-27 11:16:59 +0100

    aggiunto file di query

---
+++ aMUSE/server/sql.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ *
+ *
+ * QUERIES**
+ *
+ *
+ */
+
+
+
+exports.query_data="SELECT * FROM Object LIMIT 24";
+exports.query_select_section="";
+exports.query_select_exhibition="";
+exports.query_select_autor="";

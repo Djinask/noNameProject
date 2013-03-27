327224966dc5fd67e5b01c2b6345fd90814d77f6
LucaM <lucchard@gmail.com>
2013-03-27 17:44:53 +0100

    aggiornamento file di query anche per ricerca

--- aMUSE/server/sql.js
+++ aMUSE/server/sql.js
@@ -17,12 +17,12 @@ exports.query_select_exhibition="SELECT * FROM Exhibitions";
 exports.query_select_autor="SELECT * FROM Autors";
 exports.query_select_object="SELECT * FROM Objects WHERE id=?";
 
-exports.query_select_object_by_autor="SELECT * FROM Objects join Autors WHERE autor_id=?";
-exports.query_select_object_by_section="SELECT * FROM Objects join Sections WHERE Section_id=?";
-exports.query_select_object_by_autor="SELECT * FROM Objects join Exhibitions WHERE exhibitions_id=?";
+exports.query_select_object_by_autor="SELECT * FROM Objects inner join Autors on Object.autor=Autors.id_autor WHERE autor_id=?";
+exports.query_select_object_by_section="SELECT * FROM Objects join Sections on Object.section=Sections.id_sectioninner WHERE Section_id=?";
+exports.query_select_object_by_autor="SELECT * FROM Objects join Exhibitions on Object.exhibition=Exhibitions.id_exhibition WHERE exhibitions_id=?";
 
 
-
+exports.query_ricerca = "SELECT DISTINCT object_id, name FROM Object inner join Autors on Object.autor=Autors.id_autor inner join Sections on Object.section=Sections.id_sectioninner join Exhibitions on Object.exhibition=Exhibitions.id_exhibition WHERE autor_name LIKE ? or section_name like ? or exhibition_name like ? or name = ? or description = ?";
 
 
 

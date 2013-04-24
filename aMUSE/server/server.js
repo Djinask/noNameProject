b6852ae44281347c49916ae9444a6885cd01ac34
Kairos90 <freip@hotmail.it>
2013-04-24 09:51:45 +0200

    s added

--- aMUSE/server/server.js
+++ aMUSE/server/server.js
@@ -82,8 +82,8 @@ app.get('/photobook/bookmarks', require('./bookmarks.js'));
 app.get('/photobook/login', require('./login.js'));
 app.post('/photobook/login', require('./login.js'));
 
-app.get('/photobook/addphoto', require('./personalPhoto.js'));
-app.post('/photobook/addphoto', require('./personalPhoto.js'));
+app.get('/photobook/addphoto', require('./personalPhotos.js'));
+app.post('/photobook/addphoto', require('./personalPhotos.js'));
 app.post('/photobook/addphoto', require('./personalPhotos.js'));
 
 app.get('/photobook/logout', require('./logout.js'));
